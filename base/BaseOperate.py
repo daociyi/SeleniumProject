743b66d1186051f23b89df44733389d380ca4f1f
yuzhiwen <yuzhiwen@xmstandards.com>
2023-04-07 15:00:08 +0800

    修改项目架构

--- base/BaseOperate.py
+++ base/BaseOperate.py
@@ -128,7 +128,7 @@ class BaseOperate():
                                     img.putpixel((img_x, img_y), 255)  # 取白
                     else:
                         pass
-        result = pytesseract.image_to_string(img)
+        result = pytesseract.image_to_string(img,lang='eng')
         # 去除不相关标识符,用字典的方式 string代表查到的字符 replace_dict[string]为查到字符对应的替换
         code = ''
         for key in result:
